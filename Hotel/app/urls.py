dd37f2deec401d8339e54a4c14704de3ad2ecef2
RZN <meddrazin12@gmail.com>
2024-04-04 11:42:28 +0000

    manager/client setup

--- Hotel/app/urls.py
+++ Hotel/app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('managerlog/', views.manager_login, name='login-in'),
+  path('managerlog/', views.manager_login, name='manager-login'),
   path('managerdash/', views.manager_dash, name='manager-dash'),
+  path('clientlog/', views.manager_dash, name='client-login'),
+  path('clientdash/', views.manager_dash, name='client-dash'),
+  path("clientsignup/", views.client_signup, name="client-signup"),
 ]
--- Hotel/app/views.py
+++ Hotel/app/views.py
@@ -3,16 +3,44 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.models import User , auth
 
 # Create your views here.
+#-----------------manager----------------#
 def manager_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username = username , password = password)
-        if user is not None:
-            auth.login(request , user)
-            return redirect('manager-dash')
-    else:
+    if request.method != 'POST':
         return render(request , 'app/manager_login.html')
+    user = authenticate(request , username=request.POST['username'] , password=request.POST['password'])
+    if user is not None:
+        auth.login(request , user)
+    return redirect('manager-dash')
 
 def manager_dash(request):
     return render(request , 'app/manager_dash.html')
+
+
+#-----------------client----------------#
+def client_login(request):
+    if request.method != 'POST':
+        return render(request , 'app/client_login.html')
+    user = authenticate(request , username=request.POST['username'] , password=request.POST['password'])
+    if user is not None:
+        auth.login(request , user)
+    return redirect('client-dash')
+
+def client_signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password1']  
+        password2 = request.POST['password2']
+        
+        # Check if passwords match
+        if password != password2:
+            return render(request, 'app/client_signup.html', {'error': 'Passwords do not match'})
+
+        # Create the user
+        user = User.objects.create_user(username=username, email=email, password=password)
+        
+        user.save()
+
+        return redirect('client-login')
+
+    return render(request, 'app/client_login.html')
